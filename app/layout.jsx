5c74c4052ed4faa6f0ab0a2bde82e79b4468e6c6
SlyB33 <skvmOmega@gmail.com>
2025-02-08 15:42:17 +0100

    fix meta noindex

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,31 +4,33 @@ import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-
 const JetBrainsMono = JetBrains_Mono({
-  subsets: ["latin"],
-  weight: ['100','200','300','400','500','600','700','800'],
-  variable: "--font-jetbrainsMono",
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+	variable: "--font-jetbrainsMono",
 });
 
 const OpenSans = Open_Sans({
-  subsets: ["latin"],
-  weight: ['300','400','500','600','700','800'],
-  variable: "--font-openSans",
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "600", "700", "800"],
+	variable: "--font-openSans",
 });
 export const metadata = {
-  title: "SKDigit",
-  description: "Texte de description à rajouter",
+	// title: "SKDigit - Création de sites internet",
+	// description: "Développeur freelance basé à Lablachère en Ardèche, spécialisé en WordPress , Symfony et React. Projets web sur mesure pour Aubenas, Les Vans et alentours. Contactez-moi pour vos besoins en création de sites et applications web.",
+	// keywords: ["développeur freelance Ardèche", "développeur Wordpress Ardèche", "développeur Symfony Ardèche", "développeur React Ardèche", "création site web Lablachère", "développeur web Les Vans", "développeur web Aubenas", "freelance React Symfony WordPress", "création application web Ardèche", "création site internet Ardèche", "création application mobile Ardèche", "création site e-commerce Ardèche", "création site vitrine Ardèche", "création site sur mesure Ardèche", "création site web"],
+	robots: "noindex",
+	// authors: [{ name: "Sylvain Butet", url: "https://skdigit.com" }],
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${JetBrainsMono.variable} ${JetBrainsMono.variable} antialiased`}>
-        <Header />
-        <StairTransition />
-        <PageTransition>{children}</PageTransition>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${OpenSans.variable} ${OpenSans.variable} antialiased`}>
+				<Header />
+				<StairTransition />
+				<PageTransition>{children}</PageTransition>
+			</body>
+		</html>
+	);
 }
