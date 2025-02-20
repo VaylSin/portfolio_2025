44aed860126a888e650e712d0528d336b55f0f64
SlyB33 <skvmOmega@gmail.com>
2025-02-20 15:10:10 +0100

    fix use client and dynamix robots meta

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,11 @@
-import {
-	Open_Sans,
-	Literata as LiterataFont,
-	Roboto_Condensed,
-	Raleway as RalewayFont,
-	Oswald,
-} from "next/font/google";
+"use client";
+
+import { Raleway as RalewayFont } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import { useEffect, useState } from "react";
 
 const Raleway = RalewayFont({
 	subsets: ["latin"],
@@ -16,49 +13,48 @@ const Raleway = RalewayFont({
 	variable: "--font-RalewayFont",
 });
 
-const oswald = Oswald({
-	subsets: ["latin"],
-	weight: ["300", "400", "500", "600", "700"],
-	variable: "--font-oswald",
-});
-const literata = LiterataFont({
-	subsets: ["latin"],
-	weight: ["300", "400", "500", "600", "700"],
-	variable: "--font-literata",
-});
-export const metadata = {
-	title: "SKDigit - Création de sites internet",
-	description:
-		"Développeur freelance basé à Lablachère en Ardèche, spécialisé en WordPress , Symfony et React. Projets web sur mesure pour Aubenas, Les Vans et alentours. Contactez-moi pour vos besoins en création de sites et applications web.",
-	keywords: [
-		"développeur freelance Ardèche",
-		"développeur Wordpress Ardèche",
-		"développeur Symfony Ardèche",
-		"développeur React Ardèche",
-		"création site web Lablachère",
-		"développeur web Les Vans",
-		"développeur web Aubenas",
-		"freelance React Symfony WordPress",
-		"création application web Ardèche",
-		"création site internet Ardèche",
-		"création application mobile Ardèche",
-		"création site e-commerce Ardèche",
-		"création site vitrine Ardèche",
-		"création site sur mesure Ardèche",
-		"création site web",
-	],
-	robots: "index, follow",
-	authors: [{ name: "Sylvain Butet", url: "https://skdigit.com" }],
-	og: {
+export default function RootLayout({ children }) {
+	const [robots, setRobots] = useState("index, follow");
+
+	useEffect(() => {
+		if (
+			typeof window !== "undefined" &&
+			window.location.hostname !== "skdigit.com"
+		) {
+			setRobots("noindex, nofollow");
+		}
+	}, []);
+	const metadata = {
 		title: "SKDigit - Création de sites internet",
 		description:
 			"Développeur freelance basé à Lablachère en Ardèche, spécialisé en WordPress , Symfony et React. Projets web sur mesure pour Aubenas, Les Vans et alentours. Contactez-moi pour vos besoins en création de sites et applications web.",
-		image: "https://skdigit.com/assets/img/web_banner.png",
-		url: "https://skdigit.com",
-	},
-};
-
-export default function RootLayout({ children }) {
+		keywords: [
+			"développeur freelance Ardèche",
+			"développeur Wordpress Ardèche",
+			"développeur Symfony Ardèche",
+			"développeur React Ardèche",
+			"création site web Lablachère",
+			"développeur web Les Vans",
+			"développeur web Aubenas",
+			"freelance React Symfony WordPress",
+			"création application web Ardèche",
+			"création site internet Ardèche",
+			"création application mobile Ardèche",
+			"création site e-commerce Ardèche",
+			"création site vitrine Ardèche",
+			"création site sur mesure Ardèche",
+			"création site web",
+		],
+		robots: "index, follow",
+		authors: [{ name: "Sylvain Butet", url: "https://skdigit.com" }],
+		og: {
+			title: "SKDigit - Création de sites internet",
+			description:
+				"Développeur freelance basé à Lablachère en Ardèche, spécialisé en WordPress , Symfony et React. Projets web sur mesure pour Aubenas, Les Vans et alentours. Contactez-moi pour vos besoins en création de sites et applications web.",
+			image: "https://skdigit.com/assets/img/web_banner.png",
+			url: "https://skdigit.com",
+		},
+	};
 	return (
 		<html lang="en">
 			<body className={`${Raleway.variable} ${Raleway.variable} antialiased`}>
