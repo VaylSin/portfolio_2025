be53a5394c3d8900a34dd80f1fc9db1b11c93973
SlyB33 <skvmOmega@gmail.com>
2025-02-22 19:49:44 +0100

    fix indexation + env variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import { Raleway as RalewayFont } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
 import PageTransition from "../components/PageTransition";
 import StairTransition from "../components/StairTransition";
-import { useEffect, useState, ReactNode } from "react";
 import Script from "next/script";
 
 const Raleway = RalewayFont({
@@ -15,21 +12,16 @@ const Raleway = RalewayFont({
 });
 
 interface RootLayoutProps {
-	children: ReactNode;
+	children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
-	const [robots, setRobots] = useState("index, follow");
+// Fonction pour générer les métadonnées
+export async function generateMetadata() {
+	const hostname = process.env.NEXT_PUBLIC_HOSTNAME || "skdigit.com";
+	const robots =
+		hostname === "skdigit.com" ? "index, follow" : "noindex, nofollow";
 
-	useEffect(() => {
-		if (typeof window !== "undefined") {
-			if (window.location.hostname !== "skdigit.com") {
-				setRobots("noindex, nofollow");
-			}
-		}
-	}, []);
-
-	const metadata = {
+	return {
 		title: "SKDigit - Création de sites internet",
 		description:
 			"Développeur freelance basé à Lablachère en Ardèche, spécialisé en WordPress , Symfony et React. Projets web sur mesure pour Aubenas, Les Vans et alentours. Contactez-moi pour vos besoins en création de sites et applications web.",
@@ -50,35 +42,29 @@ export default function RootLayout({ children }: RootLayoutProps) {
 			"création site sur mesure Ardèche",
 			"création site web",
 		],
-		robots: robots,
+		robots: robots, // Utilisez la valeur de `robots` déterminée dynamiquement
 		authors: [{ name: "Sylvain Butet", url: "https://skdigit.com" }],
-		og: {
+		openGraph: {
 			title: "SKDigit - Création de sites internet et d'applications web",
 			description:
 				"Développeur freelance basé à Lablachère en Ardèche, spécialisé en WordPress , Symfony et React. Projets web sur mesure pour Aubenas, Les Vans et alentours. Contactez-moi pour vos besoins en création de sites et applications web.",
-			image: "https://skdigit.com/web_banner.png",
+			images: [
+				{
+					url: "https://skdigit.com/web_banner.png",
+					width: 1200,
+					height: 630,
+					alt: "Logo de Skdigit",
+				},
+			],
 			url: "https://skdigit.com",
+			type: "website",
 		},
 	};
+}
 
+export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
-			<head>
-				<meta name="robots" content={metadata.robots} />
-				<title>{metadata.title}</title>
-				<meta name="description" content={metadata.description} />
-				<meta name="keywords" content={metadata.keywords.join(", ")} />
-				<meta name="author" content={metadata.authors[0].name} />
-				<link rel="canonical" href={metadata.authors[0].url} />
-				<meta property="og:title" content={metadata.og.title} />
-				<meta property="og:description" content={metadata.og.description} />
-				<meta property="og:image" content={metadata.og.image} />
-				<meta property="og:image:width" content="1200" />
-				<meta property="og:image:height" content="630" />
-				<meta property="og:image:alt" content="Logo de Skdigit" />
-				<meta property="og:url" content={metadata.og.url} />
-				<meta property="og:type" content="website" />
-			</head>
 			<body className={`${Raleway.variable} ${Raleway.variable} antialiased`}>
 				<Header />
 				<StairTransition />
